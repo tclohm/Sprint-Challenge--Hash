c47e8accd0f3410954f1f7f9a66876f02d9e7a31
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-05-08 10:10:37 -0700

    finished ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -3,7 +3,17 @@ def finder(files, queries):
     """
     YOUR CODE HERE
     """
+    result = []
+    cache = {}
 
+    for query in queries:
+        if query not in cache:
+            cache[query] = False
+
+    for file in files:
+        last_file = file.split('/')[-1]
+        if last_file in cache:
+            result.append(file)
     return result
 
 
